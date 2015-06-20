2bf9b75fb7ddca69fb4b029aed893a186c34e96f
JerryLee <827596457@qq.com>
2015-06-20 22:12:52 +0900

    Database added

---
+++ app/views/books/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @book, :id, :name, :author, :isbn, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  resources :books
   get "welcome/say_hello" => "hello#say"
   get "welcome/page2" => "hello#page2"
   # The priority is based upon order of creation: first created -> highest priority.
