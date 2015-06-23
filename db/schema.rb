4eeb8e9240e1ee15bdc1d711c2b84a7fed5893e1
vagrant <vagrant@vagrant-centos65.vagrantup.com>
2015-06-23 05:59:36 +0000

    change to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150621074902) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "books", force: :cascade do |t|
     t.string   "title"
     t.string   "author"
